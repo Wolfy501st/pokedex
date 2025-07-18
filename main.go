4fb659d51147fa90dd793e0d7d044c20af6ecf8f
Joshua Wilfong <joshua14wilfong@gmail.com>
2025-07-18 15:37:14 -0400

    Refactors CLI to use REPL with external API client
    Replaces manual command parsing with a REPL pattern and introduces
    a client for interacting with an external API. Simplifies the main
    logic and improves extensibility by delegating command handling
    and configuration to separate components.

--- main.go
+++ main.go
@@ -1,75 +1,16 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
+	"time"
+
+	"github.com/Wolfy501st/pokedex/internal/pokeapi"
 )
 
-var commands map[string]cliCommand
-
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
-}
-
-func cleanInput(text string) []string {
-	input := strings.Fields(text)
-	return input
-}
-
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp() error {
-	fmt.Println("Welcome to the Pokedex!")
-	fmt.Println("Usage:")
-	fmt.Println("")
-	for _, command := range commands {
-		fmt.Printf("%s: %s\n", command.name, command.description)
-	}
-	return nil
-}
-
-func init() {
-	commands = map[string]cliCommand{
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			callback:    commandExit,
-		},
-		"help": {
-			name:        "help",
-			description: "Displays a help message",
-			callback:    commandHelp,
-		},
-	}
-}
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Print("Pokedex > ")
-	for scanner.Scan() {
-		input := cleanInput(scanner.Text())
-		if len(input) > 0 {
-			if val, ok := commands[strings.ToLower(input[0])]; !ok {
-				fmt.Println("Unknown command")
-			} else {
-				if err := val.callback(); err != nil {
-					fmt.Println("Unknown error: ", err)
-				}
-
-			}
-		}
-		fmt.Print("Pokedex > ")
+	pokeClient := pokeapi.NewClient(5 * time.Second)
+	cfg := &config{
+		pokeapiClient: pokeClient,
 	}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
-	}
+	startRepl(cfg)
 }
